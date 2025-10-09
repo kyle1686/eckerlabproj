dca86b1c34f47fd0c91ca49bc0c98265511861be
Maxwell Rosen <maxwellrosen2006@gmail.com>
2025-10-09 16:22:33 -0700

    Initial commit with main Pi script

---
+++ pi_main.py
@@ -0,0 +1,30 @@
+def main():
+    check_for_log_make_if_none()
+    take_picture()
+    mask_picture()
+    count_green_pixels()
+    add_green_pixels_to_log()
+    get_soil_moisture()
+    add_soil_moisture_to_log()
+
+def check_for_log_make_if_none():
+    TODO
+
+def take_picture():
+    TODO
+
+def mask_picture():
+    TODO
+
+def count_green_pixels():
+    TODO
+
+def add_green_pixels_to_log():
+    TODO
+
+def get_soil_moisture():
+    TODO
+
+def add_soil_moisture_to_log():
+    TODO
+
