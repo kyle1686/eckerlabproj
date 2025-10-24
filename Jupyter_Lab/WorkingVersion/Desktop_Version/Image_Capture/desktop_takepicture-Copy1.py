c7f80dfab61ac25dbfc65db75379a3230753ebf8
Maxwell Rosen <maxwellrosen2006@gmail.com>
2025-10-24 13:46:06 -0700

    merging files

--- Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Capture/desktop_takepicture-Copy1.py
+++ Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Capture/desktop_takepicture-Copy1.py
@@ -4,8 +4,8 @@ import time
 import os
 
 # Paths:
-image_save_dir = "eckerlabproj/Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Holder/Temp_Staging_Area" # Change this to temporary staging directory
-possible_types_path = "/Users/maxwellrosen/Storage/Salk_Plant_Imaging/eckerlabproj/Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Capture/possible_types.csv" # Change this to where the possible genotypes and stress list for the plates we're working with is
+image_save_dir = "/home/user300/eckerlabproj/Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Holder/Temp_Staging_Area" # Change this to temporary staging directory
+possible_types_path = "/home/user300/eckerlabproj/Jupyter_Lab/WorkingVersion/Desktop_Version/Image_Capture" # Change this to where the possible genotypes and stress list for the plates we're working with is
 
 # Temporary
 genotypes=["Salk_080561c", "Col-0", "Salk_098896", "SailSeq_201_G03", "Salk_052070", "Salk_50.E01.1", "Salk_203144", "SailSeq_5_E01.1", "Salk_102761c", "Salk_056202c", "Salk_002822c"]
@@ -16,7 +16,7 @@ def main():
     time.sleep(2)  # Allow the camera to adjust
     filename = get_file_name()
     picam2.capture_file(filename)
-    # print(f"Photo saved as {filename}") # Uncomment for testing
+    print(f"Photo saved as {filename}") # Uncomment for testing
 
 def camera_config():
     picam2 = Picamera2()
@@ -27,13 +27,13 @@ def camera_config():
 
 def get_file_name():
     os.makedirs(image_save_dir, exist_ok=True) # Ensure directory exists
-    while True:
-        genotype = get_genotype()
-        stress = get_stress()
-        print(f"\nSelected → Genotype: {genotype}, Stress: {stress}")
-        confirmation_input = print(f"\nSatisfied? (Enter to confirm, \"r\" to redo): ")
-        if confirmation_input.lower().strip() != r:
-            break
+#     while True:
+    genotype = get_genotype()
+    stress = get_stress()
+#        print(f"\nSelected → Genotype: {genotype}, Stress: {stress}")
+#        confirmation_input = print(f"\nSatisfied? (Enter to confirm, \"r\" to redo): ")
+#        if confirmation_input != "r":
+#            break
     filename = os.path.join(image_save_dir, f"{genotype}_{stress}_{get_current_time()}.jpg")
     return filename
 
