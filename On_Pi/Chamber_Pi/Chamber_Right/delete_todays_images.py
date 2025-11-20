4317beb1303f876a04bc89472110a0492bea8344
Maxwell Rosen <maxwellrosen2006@gmail.com>
2025-11-19 17:16:51 -0800

    updated path names to fix on_pi code

--- On_Pi/Chamber_Pi/Chamber_Right/delete_todays_images.py
+++ On_Pi/Chamber_Pi/Chamber_Right/delete_todays_images.py
@@ -1,9 +1,9 @@
 import sys
 # Change to repository root, so it can find the config folder with the paths inside it
-cm_repo_root = '/home/user300/Salk_Project_Folder/eckerlabproj'
+cr_repo_root = '/home/user300/Salk_Project_Folder/eckerlabproj'
 # Add config folder with paths to Python's import search path
 if cm_repo_root not in sys.path:
-    sys.path.insert(0, cl_repo_root)
+    sys.path.insert(0, cr_repo_root)
 
 import config.paths as paths
 
